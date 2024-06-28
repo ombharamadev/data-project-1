eae3d85db523847b0ac3d58a32341457a0f0275b
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-06-28 09:51:06 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import os
 
-btechfile_path=os.listdir("project1/btech/found")
-bpharmafile_path=os.listdir("project1/btech/found/bpharma/found")
-print(len(btechfile_path)+len(bpharmafile_path))
-# data=BeautifulSoup.find()
+btechfile_path=os.listdir("btech/found")
+bpharmafile_path=os.listdir("bpharma/found")
+html_files=btechfile_path + bpharmafile_path
+
+print("Total html files in both folders is ",len(html_files))
+
+for i in html_files:
+    try:
+        file=open(f"btech/found/{i}","r")
+    except:
+        file=open(f"bpharma/found/{i}","r")
+    data=file.read()
+    soup=BeautifulSoup(data)
+    data=soup.find_all("tr")
+    print(data)
+    break
+
