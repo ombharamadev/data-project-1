cdbeece4c4f9f88af95022e2c0316027aaa41fe8
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-06-29 11:56:48 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -9,13 +9,41 @@ html_files=btechfile_path + bpharmafile_path
 print("Total html files in both folders is ",len(html_files))
 
 for i in html_files:
+    if i=="20011614003_1800265728.html":
+        print("match found this exists")
     try:
         file=open(f"btech/found/{i}","r")
     except:
         file=open(f"bpharma/found/{i}","r")
     data=file.read()
-    soup=BeautifulSoup(data)
-    data=soup.find_all("tr")
+    # print(data)
+    soup=BeautifulSoup(data,features="html.parser")
+    # print(soup.findAll('td',attr={'class':'border1'}))
+    # for j in soup.find_all('td',attr={'class':'border1'}):
+    #     print("j is ",j)
+    data=soup.find('html')
+    data12=data.find('body')
+    # print(data)
+    data1=data12.find_all('div',class_="cont_outer print")
+    print(data1)
+    for row in data1:
+        try:
+            data=row.find("table",attr={'class':'cont_DIV_UN'})
+            data=data.find('tbody')
+            data=data.find('tr')
+            data=data.find('td')
+            data=data.find('div',attr={'div':'mid_PART_UN'})
+        except:
+            print("pass")
+            pass
+    # data=data.find('table')
+    # data=data.find('tbody')
+    # data=data.find('tr')
+    # data=data.find('td')
+    # data=data.find('table')
+    # data=data.find('tbody')
+    # data=data.find('tr')
+    # data=data.find_all('td',attr={'class':'border1'})
     print(data)
     break
 
