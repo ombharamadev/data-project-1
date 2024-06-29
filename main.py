633af6d64ed71c8e44c51bcd1a4c900fc8bad49b
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-06-29 12:45:05 +0000

    modified:   main.py 	new file:   result.csv

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ html_files=bpharmafile_path+btechfile_path
 
 print("Total html files in both folders is ",len(html_files))
 
+cgpa_data=[]
+sgpa_data=[]
+files_name=[]
 plus=0
 files_lack_cgpa=[]
 html0=["20011715005_1800265223.html","15619_1800265830.html","20011715004_1800265851.html","20011715004_1800265710.html"]
 
 for i in html_files:
+    files_name.append(i)
     print("file name is ->",i)
     plus=plus+1
     
@@ -33,15 +37,21 @@ for i in html_files:
                 if ("CGPA" in dat1.text)or("SGPA" in dat1.text):
                     auto_eva=auto_eval
             if "CGPA" in data1[auto_eva].text:
-                cgpa_data=data1[auto_eva].text.split(" ")[1]
-                sgpa_data=data1[auto_eva-1].text.split(" ")[1]
+                cgpa_data.append(data1[auto_eva].text.split(" ")[1])
+                sgpa_data.append(data1[auto_eva-1].text.split(" ")[1])
             else:
-                sgpa_data=data1[auto_eva].text.split(" ")[1]
-                cgpa_data=0
+                sgpa_data.append(data1[auto_eva].text.split(" ")[1])
+                cgpa_data.append(" ")
                 
     elif ("CGPA" not in content)or("SGPA" not in content):
         files_lack_cgpa.append(i)
-        cgpa_data=0
-        sgpa_data=0
-    print(cgpa_data,sgpa_data)
+        cgpa_data.append(0)
+        sgpa_data.append(0)
 print("files with not cgpa or sgpa are",len(files_lack_cgpa))
+
+array=[files_name,sgpa_data,cgpa_data]
+
+#code for writing data in csv file 
+df=pd.DataFrame(array).transpose()
+print(df.transpose())
+df.to_csv("result.csv")
